3ac0f28211e40aec8652af21fb389da01cd42952
Ehsan <ehsan_10537@Ehsans-MacBook-Air.local>
2024-12-15 18:19:57 +0330

    updated files

--- 08-GET, POST, PUT, DELETE.py
+++ 08-GET, POST, PUT, DELETE.py
@@ -4,7 +4,7 @@
 # In this example we are going to create a to-do list.
 
 
-from flask import  Flask, render_template, request, jsonify
+from flask import  Flask, render_template, request, jsonify, redirect
 
 # what is jsonify? 
 """
@@ -13,20 +13,30 @@ It converts Python data structures (like dictionaries, lists, etc.) into JSON fo
  which is a standard data-interchange format widely used in web applications.
 """
 
+# what do we do in this module?
+"""
+We are going to create a list of tasks (a to-do list) and according to this list, we will be dealing with some examples of
+GET, POST, PUT, and DELETE methods to see how they are going to be implemented.
+
+* you can test the API using postman software sending GET, POST, PUT, and DELETE requests to the server.
+* you can send along a JSON file usig postman.
+"""
+
 app = Flask(__name__)
 
-
-#Initial Data in the To-Do list
+#Initial Data in the To-Do list. This is the list of tasks we initially have.
 items =[
     {"id":1, "name":"Item 1", "description":"This is item 1"},
     {"id":2, "name":"Item 2", "description":"This is item 2"}
 ]
 
+# Home page
 @app.route('/')
 def home():
     return "Welcome to the sample To-Do list app"
 
-# GET: Retrieve all the items
+
+# GET: Retrieve all the items currently available.
 @app.route('/items', methods=['GET'])
 def get_items():
     return jsonify(items)
@@ -57,15 +67,18 @@ def create_item():
     return jsonify(new_item)
 
 # PUT : Update an existing item --> in PUT we update an item
+# PUT method will be implemented in the body of a POST method.
 @app.route('/items/<int:item_id>', methods=['PUT'])
 def update_item(item_id):
     item = next((item for item in items if item["id"]==item_id), None)
     if item is None:
         return jsonify({"Error":"item not found"})
     else:
-        item["name"] = request.json.get('name', item['name'])
+        item["name"] = request.json.get('name', item['name'])   
         item["description"] = request.json.get('description', item['description'])
-        
+        # as you can see here some data has been posted to this url as a JSON file including the updated name and description.
+        # which we are using to implement the PUT method and update an existing task or item.
+
         return jsonify(item)
 
 
@@ -77,10 +90,7 @@ def delete_item(item_id):
     return jsonify({"result":"Item deleted"})
 
 
-
 if __name__ == '__main__':
     app.run(debug=True)
 
 
-# you can test the API using postman software sending GET, POST, PUT, and DELETE requests to the server.
-# you can send along a JSON file usig postman.
